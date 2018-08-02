f07dbdb4147d7e6b0732b1d18966ecb9dc47bf18
asmaahamdy25 <sun80826@gamil.com>
2018-08-02 15:10:06 +0200

    test2

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Recipe } from '../recipe.model';
 
 @Component({
   selector: 'app-recipe-list',
@@ -6,7 +7,11 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
+  recipes: Recipe [] = [
+    new Recipe('A test Recipe ', 'this simple test ', 'https://www.logolynx.com/images/logolynx/82/829ba7822e43ebe89394d1ecbbf152b7.jpeg'),
+    new Recipe('A test Recipe ', 'this simple test ', 'https://www.logolynx.com/images/logolynx/82/829ba7822e43ebe89394d1ecbbf152b7.jpeg')
 
+  ];
   constructor() { }
 
   ngOnInit() {
